a054c63b7b8698b6bd9c0bf459e219d0be0beba6
unknown <misato4114231k@gmail.com>
2019-11-08 20:22:04 +0900

    third commit

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
   <NLink to ="/second" >Second Page </NLink>
   <br>
   <input v-model="message">
+   <img width=“2%” src = "https://dlove.jp/mezzomiki/wp-content/uploads/2018/08/happy-kuji-pixar-51.jpg">
+
 </div>
 
 
